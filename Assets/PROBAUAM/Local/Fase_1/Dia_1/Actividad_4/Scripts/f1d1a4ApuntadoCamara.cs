3941cc81a604c29669c7cabaa73b357ff5edd1bf
b00tst38p <31254314+b00tst38p@users.noreply.github.com>
2021-09-27 07:14:35 -0500

    Revert "Remake de control"
    This reverts commit b58137073021c7c38421ebe715f75d95861ef56b.

--- Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4ApuntadoCamara.cs
+++ Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4ApuntadoCamara.cs
@@ -5,82 +5,99 @@ using UnityEngine.AI;
 
 public class f1d1a4ApuntadoCamara : MonoBehaviour
 {
-    public Transform cabeza;
-    public Transform posicionInicial;
+    public new Transform camera;
     public float rayDistance;
-    private GameObject gato, iniciador;
-    //global_sc_Contador s;
+    private GameObject a, e;
+    private GameObject Iniciador;
+    global_sc_Contador s;
     private Animator animacion;
+    private f1d1a4SeguimientoPlayer seguimiento;
+    private NavMeshAgent nav;
     private AudioSource audios;
     private f1d1a4SeguimientoPelota Movimiento;
-    //public Transform teleport;
+    public Transform teleport;
     public int total;
     public GameObject Puntero;
-    private GameObject limite, Reloj;//revisar si se necesita el limite
-    public GameObject[] catToCopy;
+  
+    private Rigidbody gato;
+    private GameObject limite,Reloj;
 
     void Start()
-    {   
-        gato = GameObject.Find("cat");
-        iniciador = GameObject.Find("Iniciador");
-        limite = GameObject.Find("Limite");
-        //posicionInicial = GameObject.Find("PosicionInicial");
-
-        //s = ;
-        // cabeza = transform.Find("VRCamera");
+    {  a = GameObject.Find("cat");
+        Iniciador = GameObject.Find("Iniciador");
+        s = GameObject.Find("Contador").GetComponent<global_sc_Contador>();
+        // camera = transform.Find("VRCamera");
+        gato = a.GetComponent<Rigidbody>();
         Reloj = GameObject.Find("Canvas");
-        Movimiento = gato.GetComponent<f1d1a4SeguimientoPelota>();
+        e = GameObject.Find("Contador");
+        Movimiento = a.GetComponent<f1d1a4SeguimientoPelota>();
+        animacion = a.GetComponent<Animator>();
+        seguimiento = a.GetComponent<f1d1a4SeguimientoPlayer>();
+        nav = a.GetComponent<NavMeshAgent>();
+        nav.enabled = false;
         Movimiento.enabled = false;
-
-        animacion = gato.GetComponent<Animator>();
-        
-        gato.GetComponent<NavMeshAgent>().enabled = false;
-        
-        audios = gato.GetComponent<AudioSource>();
+        audios = a.GetComponent<AudioSource>();
         audios.enabled = false;
         animacion.SetBool("Sentar", true);
         animacion.SetBool("Entrecaminando", false);
         animacion.SetBool("Caminar", false);
-        gato.GetComponent<f1d1a4SeguimientoPlayer>().enabled = false;
-        //contador.SetActive(false);
+        limite = GameObject.Find("Limite");
+        seguimiento.enabled = false;
+        e.SetActive(false);
+
+     
+
     }
+
 
     void Update()
     { 
         RaycastHit hit;
-        if (limite.GetComponent<f1d1a4_s_Contador>().getContador() != total)
+        if (s.getContador() != total)
         {
-            Debug.DrawRay(cabeza.position, cabeza.forward * rayDistance, Color.red);
+           
+           
+
+
+            Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
             //Lerp
-            if (Physics.Raycast(cabeza.position, cabeza.forward, out hit, rayDistance))
+
+
+            if (Physics.Raycast(camera.position, camera.forward, out hit, rayDistance))
             {
                 Puntero.SetActive(true);
 
                 Debug.Log(hit.transform.name);
 
                 Puntero.transform.position = hit.point;
-                if (hit.transform.name == iniciador.name)
+
+
+
+                if (hit.transform.name == Iniciador.name)
                 {
 
                     //si apunta al   gato
-                    iniciador.SetActive(false);
-                    gato.GetComponent<BoxCollider>().enabled = true;
- 
-                    Debug.Log("Gato listo para caminar");
+                    Iniciador.SetActive(false);
+                    a.GetComponent<BoxCollider>().enabled = true;
+
+                    Debug.Log("Entro");
 
                     if (Movimiento.enabled == false)
                     {
-                        gato.GetComponent<Rigidbody>().transform.Rotate(new Vector3(90, 0, 0));
+                        gato.transform.Rotate(new Vector3(90, 0, 0));
                         Movimiento.enabled = true;
                         limite.SetActive(true);
                         Reloj.GetComponent<global_sc_Temporizador>();
-                        ////que hace con el reloj aquí?
+
+
+
+
                     }
 
                 }
                 if (hit.transform.name == "Tree_03")
                 {
-                    if (iniciador.activeInHierarchy == true)
+                    if (Iniciador.activeInHierarchy == true)
                     {
                         //no hace nada
                         Debug.Log("Desactiva el iniciador");
@@ -91,77 +108,98 @@ public class f1d1a4ApuntadoCamara : MonoBehaviour
                         animacion.SetBool("Sentar", false);
                         animacion.SetBool("Caminar", true);
                         Movimiento.enabled = true;
-                        Movimiento.caminar = true;
+                        Movimiento.condicion = true;
+
+
+
+
                     }
+
                 }
                 else
                 {
                     Movimiento.enabled = false;
-                    Movimiento.caminar = false;
+                    Movimiento.condicion = false;
+                    // Iniciador.SetActive(true); 
+
                 }
+                
 
 
 
-                if (hit.transform.name == limite.name)
+
+
+
+
+                if (hit.transform.name == e.name)
                 {//toco al contador
                     Movimiento.enabled = false;
 
-                    //contador.SetActive(false);
-                    iniciador.SetActive(true);
+                    e.SetActive(false);
+                    Iniciador.SetActive(true);
 
-                    //Vector3 newpos = new Vector3(0.335f, 10.196f, 21.813f);
+                    Vector3 newpos = new Vector3(0.335f, 10.196f, 21.813f);
                     //newpos.x = 0.335f; newpos.y = 10.33f; newpos.z = 21.813f;
-                    
 
-                    //int rand = Random.Range(0,2);
-                    //Debug.Log("gato " + rand);
+                    Rigidbody clon = (Rigidbody)Instantiate(a.GetComponent<Rigidbody>(), newpos, a.transform.rotation); 
+                    clon.GetComponent<BoxCollider>().isTrigger = false;
+                    seguimiento.enabled = false;
+                  
+                    nav.enabled = false;
+                    a.transform.position = newpos;
+                    clon.GetComponent<Animator>().SetBool("Sentar", false);
+                    clon.GetComponent<Animator>().SetBool("Caminar", true);
+                    clon.GetComponent<Animator>().SetBool("Entrecaminando", true);
+                    a.GetComponent<Animator>().SetBool("Entrecaminando", false);
+                    a.GetComponent<Animator>().SetBool("Sentar", false); ;
+                    a.GetComponent<Animator>().SetBool("Caminar", false);
+                    s.suma();
+                    //  
 
-                    //Rigidbody clon = (Rigidbody)Instantiate(catToCopy[rand].GetComponent<Rigidbody>(), posicionInicial.position, gato.transform.rotation); 
-                    
-                    //clon.GetComponent<Animator>().SetBool("Sentar", false);
-                    //clon.GetComponent<Animator>().SetBool("Caminar", false);
-                    //clon.GetComponent<Animator>().SetBool("Entrecaminando", false);
 
 
-                    //gato.GetComponent<BoxCollider>().isTrigger = false;
 
-                    //gato.GetComponent<f1d1a4SeguimientoPlayer>().enabled = false;
-                    //gato.GetComponent<NavMeshAgent>().enabled = false;
-                    /*gato.transform.position = posicionInicial.position;
-                    
-                    gato.GetComponent<Animator>().SetBool("Entrecaminando", true);
-                    gato.GetComponent<Animator>().SetBool("Sentar", false); ;
-                    gato.GetComponent<Animator>().SetBool("Caminar", true);
-                    limite.GetComponent<global_sc_Contador>().suma();*/
+
                 }
+                
 
-                if (hit.transform.name != "Tree_03")
+
+                    if (hit.transform.name != "Tree_03")
                 {
-                    if (iniciador.activeInHierarchy==false) { 
-                        animacion.enabled = true;
-                        animacion.SetBool("Caminar", false);
-                        animacion.SetBool("Entrecaminando", false);
-                        animacion.SetBool("Sentar", true);
+                    if (Iniciador.activeInHierarchy==false) { 
+                    animacion.enabled = true;
+                    animacion.SetBool("Caminar", false);
+                    animacion.SetBool("Entrecaminando", false);
+                    animacion.SetBool("Sentar", true);
                     }
                 }
+
+                  
+
+
             }
             else{
              
+             
                 if (hit.collider==null) {
                     Puntero.SetActive(false);
-                    //   Vector3 posicion = cabeza.transform.position;
+                    //   Vector3 posicion = camera.transform.position;
                     //posicion.z = posicion.z + 5;
                     //Puntero.transform.position = posicion;
+                   
                 }
                 //seguir al laser aunque no apuntes un objeto
+                
              }
         }
         else {
             animacion.enabled = false;
-            gato.SetActive(false);
+            a.SetActive(false);
             Reloj.GetComponent<global_sc_Temporizador>().enabled = false;
         }
+
     }
+    //_____---------------------
 }
    
    
--- Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4SeguimientoPlayer.cs
+++ Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4SeguimientoPlayer.cs
@@ -5,48 +5,74 @@ using UnityEngine.AI;
 
 public class f1d1a4SeguimientoPlayer : MonoBehaviour
 {
-    private Transform player;
+    public Transform player;
     public Animator Animacion;
     public NavMeshAgent nav;
+    private GameObject seguimiento;
+    private GameObject desaparecer;
+    private BoxCollider des;
     private bool cont;
-    public f1d1a4SeguimientoPelota movimiento;
+    //ScriptVRCamera c;
+   public f1d1a4SeguimientoPelota movimiento;
     public AudioSource audios;
-    
     void Start()
     {
-        movimiento.caminar = false;
+        movimiento.condicion = false;
+        seguimiento =GameObject.Find("Iniciar");
+        //   player = GameObject.Find("Player");
         player = GameObject.FindGameObjectWithTag("Player").transform;
         nav.GetComponent<NavMeshAgent>();
+        desaparecer = GameObject.Find("Bench");
         nav.enabled = true;
+        des = desaparecer.GetComponent<BoxCollider>();
         Animacion.SetBool("Entrecaminando", false);
         Animacion.SetBool("Sentar", false);
         Animacion.SetBool("Caminar", true);
+        // Animacion.SetBool("Caminar",true);
         audios.enabled = true;
+        cont=true;
+       // audios.enabled = false;
     }
     
+
+    // Update is called once per frame
     void Update()
     {
+
         if (nav.enabled == true)
         {
+
             nav.SetDestination(player.position);
         }
+
         else if (nav.enabled == false) { 
-            Animacion.SetBool("Sentar", true);
+              Animacion.SetBool("Sentar", true);
             Animacion.SetBool("Caminar", false);
             Animacion.SetBool("Entrecaminando", false);
-            Animacion.enabled = true;  
+            Animacion.enabled = true;
+           
         }
+        
+        
+       
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "PosicionFinal") {
-                audios.enabled = false;
+       
+        if (other.name== "Bench") {
+            audios.enabled = false;
             if (Animacion.enabled == true) { 
-                Animacion.enabled = false;
+            Animacion.enabled = false;
                 nav.enabled = false;
                 audios.enabled = true;
             }
+
+
+
+
         }
+        
+        
     }
+
 }
