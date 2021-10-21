3aee0bfbe6a6ac01f2a489ad8fdb72eb9d33245f
b00tst38p <31254314+b00tst38p@users.noreply.github.com>
2021-10-21 01:08:08 -0500

    Completada funcionalidad general del juego
    Se crean gatos consecutivamente
    Tres diseños de gatos seleccionados aleatoriamente
    Se forman frente al usuario

--- Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4_s_ControlGato.cs
+++ Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4_s_ControlGato.cs
@@ -13,16 +13,25 @@ public class f1d1a4_s_ControlGato : MonoBehaviour
     public GameObject limite;
     public GameObject player;
     public Animator animacion;
+    public Vector3 posicionMeta;
+    public GameObject controlEscena;
+    public int gatoID=0;
     
     // Start is called before the first frame update
     void Start()
     {
-        puntero = GameObject.Find("Puntero");
-        GameObject.Find("ControlEscena").GetComponent<f1d1a4_s_Puntero>().gato = gameObject;
+        
+    }
+
+    void Awake(){
+        //puntero = GameObject.Find("Puntero");
+        //controlEscena = GameObject.Find("controlEscena");
+        //controlEscena.GetComponent<f1d1a4_s_Puntero>().gato = gameObject;/// quitar esta linea cuando ya se creen automáticamente
         //GameObject.Find("ControlEscena").GetComponent<f1d1a4_s_Puntero>().animacion = gameObject.GetComponent<Animator>();
-        limite = GameObject.Find("Limite");
-        player = GameObject.Find("Global_s_Player");
-        animacion = gameObject.GetComponent<Animator>();
+        //limite = GameObject.Find("Limite");
+        //player = GameObject.Find("Global_s_Player");
+        //animacion = gameObject.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -37,11 +46,6 @@ public class f1d1a4_s_ControlGato : MonoBehaviour
             {
                 transform.position = Vector3.Lerp(transform.position,puntero.transform.position,Time.deltaTime*speed);
             }
-            else
-            {
-                gameObject.GetComponent<NavMeshAgent>().enabled=true;
-                gameObject.GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
-            }
         }
         else{
                 animacion.SetBool("Entrecaminando", false);
@@ -54,10 +58,33 @@ public class f1d1a4_s_ControlGato : MonoBehaviour
     {
         if(other.transform.name == limite.name)
         {
+            posicionMeta = controlEscena.GetComponent<f1d1a4_s_Puntero>().metas[gatoID].transform.position;
+            gameObject.GetComponent<NavMeshAgent>().enabled=true;
+            gameObject.GetComponent<NavMeshAgent>().SetDestination(posicionMeta);
+            
             siguePuntero = false;
+            caminarArbol = false;
             caminarSuelo = true;
-            limite.SetActive(false);
-            GameObject.Find("Iniciador").SetActive(true);
+            controlEscena.GetComponent<f1d1a4_s_Puntero>().nuevo_Gato();
+            limite.GetComponent<BoxCollider>().enabled = false;
+            controlEscena.GetComponent<f1d1a4_s_Puntero>().iniciador.GetComponent<BoxCollider>().enabled = true;
+            
+            
+        }
+        //if(other.transform.name == GameObject.Find("Meta").name){
+            //Debug.Log(gameObject.GetComponent<NavMeshAgent>().isStopped + " " + gameObject.GetComponent<NavMeshAgent>().remainingDistance + " " + gatoID);
+        
+    }
+
+    private void OnTriggerStay(Collider other){
+        //Debug.Log(gameObject.GetComponent<NavMeshAgent>().isStopped + " " + gameObject.GetComponent<NavMeshAgent>().remainingDistance + " " + gatoID);
+        if(other.transform.name == GameObject.Find("Meta").name){
+            if(gameObject.GetComponent<NavMeshAgent>().stoppingDistance > gameObject.GetComponent<NavMeshAgent>().remainingDistance){
+                Debug.Log("stopped gato" + gatoID + " toco " + other.name);
+
+                //int i = gameObject.GetComponent<NavMeshAgent>().remainingDistance;
+                caminarSuelo = false;
+            }
         }
     }
 }
--- Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4_s_Puntero.cs
+++ Assets/PROBAUAM/Local/Fase_1/Dia_1/Actividad_4/Scripts/f1d1a4_s_Puntero.cs
@@ -7,9 +7,11 @@ public class f1d1a4_s_Puntero : MonoBehaviour
     public GameObject puntero;
     public GameObject gato; //se auto asigna cuando se crea
     public Transform cabeza;
-
+    public int contadorGatos = 0;
     public GameObject iniciador;
-    
+    public GameObject[] metas;
+    public GameObject spawn;
+    public GameObject[] catToCopy;
 
     public float rayDistance;
 
@@ -19,29 +21,30 @@ public class f1d1a4_s_Puntero : MonoBehaviour
         puntero = GameObject.Find("Puntero");
         cabeza = GameObject.Find("FollowHead").transform;
         iniciador = GameObject.Find("Iniciador");
+        spawn = GameObject.Find("Spawn");
+        nuevo_Gato();
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        Debug.DrawRay(cabeza.position, cabeza.forward * rayDistance, Color.red);
+        //Debug.DrawRay(cabeza.position, cabeza.forward * rayDistance, Color.red);
         if (Physics.Raycast(cabeza.position, cabeza.forward, out hit, rayDistance))
         {
             puntero.transform.position = hit.point;
-            Debug.Log(hit.transform.name);
+            //Debug.Log(hit.transform.name);
 
             if(hit.transform.name == iniciador.name)
             {
-                iniciador.SetActive(false);
+                iniciador.GetComponent<BoxCollider>().enabled = false;
                 gato.GetComponent<BoxCollider>().enabled = true;
                 gato.GetComponent<Rigidbody>().transform.Rotate(new Vector3(90, 0, 0));
-                GameObject.Find("Limite").SetActive(true);
-                Debug.Log("Gato listo para caminar");
+                GameObject.Find("Limite").GetComponent<BoxCollider>().enabled = true;
             }
             if(hit.transform.name == "Tree_03")
             {
-                if(!iniciador.activeInHierarchy)
+                if(!iniciador.GetComponent<BoxCollider>().enabled)
                 {
                     gato.GetComponent<f1d1a4_s_ControlGato>().caminarArbol = true;
                 }
@@ -55,5 +58,20 @@ public class f1d1a4_s_Puntero : MonoBehaviour
         {
             gato.GetComponent<f1d1a4_s_ControlGato>().caminarArbol = false;
         }
+    }
+
+    public void nuevo_Gato(){
+        
+        int rand = Random.Range(0,3);
+        Debug.Log("nuevo gato " + rand);
+        gato = Instantiate(catToCopy[rand], spawn.transform.position, spawn.transform.rotation);
+        f1d1a4_s_ControlGato control = gato.GetComponent<f1d1a4_s_ControlGato>();
+        control.puntero = puntero;
+        control.limite = GameObject.Find("Limite");
+        control.player = GameObject.Find("Global_s_Player");
+        control.animacion = gato.GetComponent<Animator>();
+        control.controlEscena = GameObject.Find("ControlEscena");
+        control.gatoID=contadorGatos;
+        contadorGatos++;
     }
 }
